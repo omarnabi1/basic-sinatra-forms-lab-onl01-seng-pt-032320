01169c0eb1e7e99a0da289ec20b4cac9b72a5386
Omar Nabi <onabi20@gmail.com>
2020-07-20 05:34:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ class App < Sinatra::Base
 
 
   get "/newteam" do 
+<<<<<<< HEAD
+=======
+    erb :newteam
+  end 
+  
+  
+  post "/team" do
+    
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    
+    erb :team
+end
+>>>>>>> 458113fb7d245efd05119902933f70d91a157928
     
 
 end
